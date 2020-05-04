642548b9bb9d5e7f7b44a27201e85373bf10f304
csdhv9 <csdhv9@umsystem.edu>
2020-05-03 23:52:29 -0500

    Initial commit.

---
+++ CudaNN/kernel.cu
@@ -0,0 +1,682 @@
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+#include <string>
+#include <fstream>
+#include <cuchar>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <ctime>
+
+
+using namespace std;
+
+typedef unsigned char uchar;
+
+/*
+TODO
+- Iterative backpropogation
+
+- use & for variables?
+- Readme
+- make sure the .user not commited and no inofo in the vcxproj(?)
+
+- Memory leaks C++ and CUDA
+- Template everything
+- Better use of CUDA
+- Make object oriented
+*/
+
+template <typename T>
+cudaError_t two_in_one_out(void (*func)(T*, const T*, const T*), T *c, const T *a, const T *b, unsigned int size);
+template <typename T>
+cudaError_t one_in_one_out(void(*func)(T*, const T*), T *b, const T *a, unsigned int size);
+
+
+__global__ void sigmoid(double *output, const double *input){
+	/*
+	Sigmoid activation function.
+
+	Parameters
+	----------
+	output: double*
+		Output array.
+	input: double*
+		Input array.
+
+	Effects
+	-------
+	Apply sigmoid function to all inputs and update output.
+	*/
+	int i = threadIdx.x;
+
+	output[i] = tanh(input[i]);
+}
+
+__global__ void dsigmoid(double *output, const double *input){
+	/*
+	Derivative of sigmoid activation function.
+
+	Parameters
+	----------
+	output: double*
+		Output array.
+	input: double*
+		Input array.
+
+	Effects
+	-------
+	Apply sigmoid function to all inputs and update output.
+	*/
+	int i = threadIdx.x;
+
+	output[i] = 1.0 - pow(input[i], 2);
+}
+
+double* onehot(double target, int N_CLASS) {
+	/*
+	Create onehot vector.
+
+	Parameters
+	----------
+	target: double
+		Value to convert.
+	N_CLASS: int
+		Number of classes.
+
+	Returns
+	-------
+	double* Onehot vector.
+	*/
+	double *output = new double[N_CLASS];
+
+	for (int i = 0; i < N_CLASS; i++) {
+		if (i == target) {
+			output[i] = 1.;
+		}
+		else {
+			output[i] = 0.;
+		}
+	}
+
+	return output;
+}
+
+int amax(double *real, const unsigned int n_values) {
+	/*
+	Argmax function.
+
+	Parameters
+	----------
+	real: double*
+		Array to find argmax of.
+	n_values: uint
+		Number of values in real.
+
+	Returns
+	-------
+	int Index of maximum value.
+	*/
+	double max = -9999;
+	int m_idx = 0;
+
+	for (unsigned int i = 0; i < n_values; i++) {
+		if (real[i] > max) {
+			max = real[i];
+			m_idx = i;
+		}
+	}
+
+	return m_idx;
+}
+
+double *matmul(const double *x, double **mat, const unsigned int maty, const unsigned int matx){
+	/*
+	Matrix multiplication
+
+	Parameters
+	----------
+	x: double[n]
+	mat: double[m, n]
+	maty: int = m
+	matx: int = n
+
+	Returns
+	-------
+	double[m] Output of matrix multiplication.
+	*/
+	double *output = new double[matx];
+
+	for (unsigned int i = 0; i < matx; i++) {
+		double temp = 0;
+
+		for (unsigned int j = 0; j < maty; j++) {
+			temp += x[j] * mat[j][i];
+		}
+
+		output[i] = temp;
+	}
+
+	return output;
+}
+
+double **forward(double *x, double ***w, const unsigned int *layers, const unsigned int n_layers){
+	/*
+	Forward propogate input through network.
+
+	Parameters
+	----------
+	x: double[n]
+		Input vector.
+	w: double[m, n]
+		Weight matrix.
+	layers: uint*
+		Size of each layer in the network.
+	n_layers: uint
+		Number of layers
+
+	Returns
+	-------
+	double** Fires in each layer of the network.
+	*/
+	double **fires = new double*[n_layers];
+	fires[0] = x;
+
+	for (unsigned int i = 0; i < n_layers - 1; i++) {
+		double *xx = matmul(fires[i], w[i], layers[i], layers[i+1]);
+		
+	
+		fires[i + 1] = new double[layers[i + 1]];
+		one_in_one_out(sigmoid, fires[i+1], xx, layers[i + 1]);
+	}
+
+	return fires;
+}
+
+double cross_entropy_loss(double *real, const double target, const unsigned int n_outputs){
+	/*
+	Cross entropy loss function.
+
+	Parameters
+	----------
+	real: double[n]
+		Approximate label.
+	target: double[n]
+		Expected label.
+	n_outputs: uint = n
+		Number of values in arrays.
+
+	Returns
+	-------
+	Cross entropy loss.
+	*/
+	double total = 0;
+	for (unsigned int j = 0; j < n_outputs; j++){
+		total += exp(real[j]);
+	}
+
+	return -real[(int) target] + log(total);
+}
+
+double *dcross_entropy_loss(const double *real, const double *target, const unsigned int n_outputs){
+	/*
+	Derivative of cross entropy loss function.
+
+	Parameters
+	----------
+	real: double[n]
+		Approximate label.
+	target: double[n]
+		Expected label.
+	n_outputs: uint = n
+		Number of values in arrays.
+
+	Returns
+	-------
+	Derivative of cross entropy loss.
+	*/
+
+	double *output = new double[n_outputs];
+
+	for (unsigned int i = 0; i < n_outputs; i++)
+		output[i] = (real[i] - target[i]) / n_outputs;
+
+	return output;
+}
+
+void backward(double ***w, const double *target, double **fires, const unsigned int *layers, const unsigned int n_layers, double learning_rate) {
+	/*
+	Cross entropy loss function.
+
+	Parameters
+	----------
+	w: double[m, n]
+		Weight matrix.
+	target: double[n]
+		Expected label.
+	fires: double[n_layers, layers]
+		Output of each layer in the network.
+	layers: uint*
+		Number of neurons in each layer.
+	n_layers: uint
+		Number of layers.
+	learning_rate: double
+		Learning rate of the network.
+
+	Effects
+	-------
+	Update weight matrix.
+	*/
+
+	double *error = dcross_entropy_loss(fires[n_layers - 1], target, layers[n_layers - 1]);
+
+	double **deltas = new double*[n_layers - 1];
+
+	double *activation_prime = new double[layers[n_layers - 1]];
+	one_in_one_out(dsigmoid, activation_prime, fires[n_layers - 1], layers[n_layers - 1]);
+
+	// dsig * error * fires[-2].reshape((-1, 1))
+	double *delta = new double[layers[n_layers - 1]];
+	for (unsigned int x = 0; x < layers[n_layers - 1]; x++) {
+		delta[x] = activation_prime[x] * error[x];
+	}
+
+	deltas[n_layers - 2] = delta;
+
+	//// make iterative
+	double *error2 = new double[layers[n_layers - 2]]; // np.sum(deltas[n_layers - 2] * self.w[i + 1], axis = 1)  //// TODO TODO TODO
+	for (int i = 0; i < layers[n_layers - 2]; i++)
+	{
+		error2[i] = 0;
+
+	for (int j = 0; j < layers[n_layers - 1]; j++) {
+		error2[i] += w[1][i][j] * delta[j];
+		}
+	}
+	double *activation_prime2 = new double[layers[n_layers - 2]];
+	one_in_one_out(dsigmoid, activation_prime2, fires[n_layers - 2], layers[n_layers - 2]);
+
+	// dsig * error
+	double *delta2 = new double[layers[n_layers - 2]];
+	for (unsigned int x = 0; x < layers[n_layers - 2]; x++) {
+		delta2[x] = activation_prime2[x] * error2[x];
+	}
+
+	deltas[n_layers - 3] = delta2;
+
+	/*
+	// Backpropogate errors
+	for i in range(len(self.layers) - 3, -1, -1) :
+		error = np.sum(deltas[0] * self.w[i + 1], axis = 1)
+
+		delta = activation_map[self.layer_activations[i]]["f '"](fires[i + 1]) * error * fires[i-2].reshape((-1, 1))
+
+		deltas.insert(0, delta)
+	*/
+	// Apply deltas
+	for (int i = n_layers - 2; i > -1; i--)
+		for (unsigned int j = 0; j < layers[i]; ++j)
+			for (unsigned int k = 0; k < layers[i + 1]; k++) {
+				w[i][j][k] -= learning_rate * deltas[i][k] * fires[i][j];
+				//printf("%f %f\n", deltas[i][k], fires[i][j]);
+			}
+}
+
+uchar** read_mnist_images(string full_path, int& number_of_images, int& image_size){
+	auto reverseInt = [](int i) {
+		unsigned char c1, c2, c3, c4;
+		c1 = i & 255, c2 = (i >> 8) & 255, c3 = (i >> 16) & 255, c4 = (i >> 24) & 255;
+		return ((int)c1 << 24) + ((int)c2 << 16) + ((int)c3 << 8) + c4;
+	};
+
+	ifstream file(full_path, ios::binary);
+
+	if (file.is_open()) {
+		int magic_number = 0, n_rows = 0, n_cols = 0;
+
+		file.read((char *)&magic_number, sizeof(magic_number));
+		magic_number = reverseInt(magic_number);
+
+		if (magic_number != 2051) throw runtime_error("Invalid MNIST image file!");
+
+		file.read((char *)&number_of_images, sizeof(number_of_images)), number_of_images = reverseInt(number_of_images);
+		file.read((char *)&n_rows, sizeof(n_rows)), n_rows = reverseInt(n_rows);
+		file.read((char *)&n_cols, sizeof(n_cols)), n_cols = reverseInt(n_cols);
+
+		image_size = n_rows * n_cols;
+
+		uchar** _dataset = new uchar*[number_of_images];
+		for (int i = 0; i < number_of_images; i++) {
+			_dataset[i] = new uchar[image_size];
+			file.read((char *)_dataset[i], image_size);
+		}
+		return _dataset;
+	}
+	else {
+		throw runtime_error("Cannot open file `" + full_path + "`!");
+	}
+}
+
+uchar* read_mnist_labels(string full_path, int& number_of_labels){
+	auto reverseInt = [](int i) {
+		unsigned char c1, c2, c3, c4;
+		c1 = i & 255, c2 = (i >> 8) & 255, c3 = (i >> 16) & 255, c4 = (i >> 24) & 255;
+		return ((int)c1 << 24) + ((int)c2 << 16) + ((int)c3 << 8) + c4;
+	};
+
+	typedef unsigned char uchar;
+
+	ifstream file(full_path, ios::binary);
+
+	if (file.is_open()) {
+		int magic_number = 0;
+		file.read((char *)&magic_number, sizeof(magic_number));
+		magic_number = reverseInt(magic_number);
+
+		if (magic_number != 2049) throw runtime_error("Invalid MNIST label file!");
+
+		file.read((char *)&number_of_labels, sizeof(number_of_labels)), number_of_labels = reverseInt(number_of_labels);
+
+		uchar* _dataset = new uchar[number_of_labels];
+		for (int i = 0; i < number_of_labels; i++) {
+			file.read((char*)&_dataset[i], 1);
+		}
+		return _dataset;
+	}
+	else {
+		throw runtime_error("Unable to open file `" + full_path + "`!");
+	}
+}
+
+
+int main(){
+	cudaError_t cudaStatus;
+
+	//// Setup Network
+	const unsigned int n_layers = 3;
+	const unsigned int layers[n_layers] = {784, 32, 10};
+
+	double learning_rate = .0001;
+
+	double ***w = new double**[n_layers - 1];
+
+	for (unsigned int i = 0; i < n_layers - 1; i++)
+	{
+		double **matrix = new double*[layers[i]];
+		for (unsigned int j = 0; j < layers[i]; ++j) {
+			matrix[j] = new double[layers[i + 1]];
+
+			for (unsigned int k = 0; k < layers[i + 1]; k++) {
+				matrix[j][k] = ((rand() % 100) / 100.) / 5. - .1;
+
+			}
+		}
+
+		w[i] = matrix;
+	}
+
+	//// Training
+
+	int N_TRAIN = 60000;
+	int N_TEST = 10000;
+	int IMG_SIZE = 784;
+
+	string TRAIN_LABEL_PATH = "C:\\MNIST\\train-labels.idx1-ubyte";
+	string TRAIN_IMAGE_PATH = "C:\\MNIST\\train-images.idx3-ubyte";
+	string TEST_LABEL_PATH = "C:\\MNIST\\t10k-labels.idx1-ubyte";
+	string TEST_IMAGE_PATH = "C:\\MNIST\\t10k-images.idx3-ubyte";
+
+	uchar *temp_train_labels = read_mnist_labels(TRAIN_LABEL_PATH, N_TRAIN);
+	uchar **temp_train_images = read_mnist_images(TRAIN_IMAGE_PATH, N_TRAIN, IMG_SIZE);
+
+	double *train_labels = new double[N_TRAIN];
+	double **train_images = new double*[N_TRAIN];
+	for (int i = 0; i < N_TRAIN; i++) {
+		train_labels[i] = (double)(temp_train_labels[i]);
+
+		train_images[i] = new double[IMG_SIZE];
+		for (int j = 0; j < IMG_SIZE; j++) {
+			train_images[i][j] = (double)(temp_train_images[i][j]) / 255.;
+		}
+	}
+
+	int N_CLASS = 10;
+
+	unsigned int N_EP = 5;
+	for (unsigned int e = 0; e < N_EP; e++) {
+		double total_error = 0; 
+		clock_t begin = clock();
+
+		for (unsigned int i = 0; i < N_TRAIN; i++) {
+			double **fires = forward(train_images[i], w, layers, n_layers);
+
+			double *target = onehot(train_labels[i], N_CLASS);
+			backward(w, target, fires, layers, n_layers, learning_rate);
+
+			total_error += cross_entropy_loss(fires[n_layers-1], train_labels[i], N_CLASS);
+		}
+		clock_t end = clock();
+		double secs = double(end - begin) / CLOCKS_PER_SEC;;
+		
+		printf("%i(%f s): %f\n", e, secs, total_error);
+	}
+	
+	
+
+	//// TEST
+	uchar *temp_test_labels = read_mnist_labels(TEST_LABEL_PATH, N_TEST);
+	uchar **temp_test_images = read_mnist_images(TEST_IMAGE_PATH, N_TEST, IMG_SIZE);
+
+	double *test_labels = new double[N_TEST];
+	double **test_images = new double*[N_TEST];
+	for (int i = 0; i < N_TEST; i++) {
+		test_labels[i] = (double)(temp_test_labels[i]);
+		
+		test_images[i] = new double[IMG_SIZE];
+		for (int j = 0; j < IMG_SIZE; j++)
+			test_images[i][j] = (double)(temp_test_images[i][j]) / 255.;
+	}
+
+	int total = 0;
+	int n_right = 0;
+	for (unsigned int i = 0; i < N_TEST; i++) {
+		double **fires = forward(test_images[i], w, layers, n_layers);
+
+		double real = (double) amax(fires[n_layers - 1], N_CLASS);
+		double target = test_labels[i];
+
+		total += 1;
+		if (real == target) {
+			n_right += 1;
+		}
+	}
+	printf("Correct: %f", ((float)n_right) / ((float)total));
+
+	//// Cleanup
+	// Exit profiling and tracing tools 
+    cudaStatus = cudaDeviceReset();
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaDeviceReset failed!");
+        return 1;
+    }
+
+    return 0;
+}
+
+template <typename T>
+cudaError_t two_in_one_out(void(*func)(T*, const T*, const T*), T *c, const T *a, const T *b, unsigned int size){
+	/*
+	CUDA function wrapper.
+
+	Parameters
+	----------
+	c: T*
+		Output.
+	a: T*
+		Input.
+	b: T*
+		Input.
+	size: uint
+		Number of values in a, b and c.
+	*/
+	int *dev_a = 0;
+    int *dev_b = 0;
+    int *dev_c = 0;
+    cudaError_t cudaStatus;
+
+    // Select GPU
+    cudaStatus = cudaSetDevice(0);
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaSetDevice failed!  Do you have a CUDA-capable GPU installed?");
+        goto Error;
+    }
+
+
+	// Allocate GPU Buffers
+	// out
+    cudaStatus = cudaMalloc((void**)&dev_c, size * sizeof(int));
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaMalloc failed!");
+        goto Error;
+    }
+
+	// in
+    cudaStatus = cudaMalloc((void**)&dev_a, size * sizeof(int));
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaMalloc failed!");
+        goto Error;
+    }
+
+	// in
+    cudaStatus = cudaMalloc((void**)&dev_b, size * sizeof(int));
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaMalloc failed!");
+        goto Error;
+    }
+
+    // Copy inputs from Host -> GPU Buffer
+    cudaStatus = cudaMemcpy(dev_a, a, size * sizeof(int), cudaMemcpyHostToDevice);
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaMemcpy failed!");
+        goto Error;
+    }
+
+    cudaStatus = cudaMemcpy(dev_b, b, size * sizeof(int), cudaMemcpyHostToDevice);
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaMemcpy failed!");
+        goto Error;
+    }
+
+    // Launch kernel w/ one thread per element.
+	func<<<1, size>>>(dev_c, dev_a, dev_b);
+
+    // Get kernel launch errors
+    cudaStatus = cudaGetLastError();
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "addKernel launch failed: %s\n", cudaGetErrorString(cudaStatus));
+        goto Error;
+    }
+
+	// Wait for kernel finish
+	cudaStatus = cudaDeviceSynchronize();
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaDeviceSynchronize returned error code %d after launching addKernel!\n", cudaStatus);
+        goto Error;
+    }
+
+	// Copy outputs from GPU Buffer -> Host
+    cudaStatus = cudaMemcpy(c, dev_c, size * sizeof(int), cudaMemcpyDeviceToHost);
+    if (cudaStatus != cudaSuccess) {
+        fprintf(stderr, "cudaMemcpy failed!");
+        goto Error;
+    }
+
+Error:
+    cudaFree(dev_c);
+    cudaFree(dev_a);
+    cudaFree(dev_b);
+    
+    return cudaStatus;
+}
+
+
+template <typename T>
+cudaError_t one_in_one_out(void(*func)(T*, const T*), T *b, const T *a, unsigned int size){
+	/*
+	CUDA function wrapper.
+
+	Parameters
+	----------
+	b: T*
+		Output.
+	a: T*
+		Input.
+	size: uint
+		Number of values in a and b.
+	*/
+	double *dev_a = 0;
+	double *dev_b = 0;
+	cudaError_t cudaStatus;
+
+	// Select GPU
+	cudaStatus = cudaSetDevice(0);
+	if (cudaStatus != cudaSuccess) {
+		fprintf(stderr, "cudaSetDevice failed!  Do you have a CUDA-capable GPU installed?");
+		goto Error;
+	}
+
+
+	// Allocate GPU Buffers
+	// out
+	cudaStatus = cudaMalloc((void**)&dev_b, size * sizeof(double));
+	if (cudaStatus != cudaSuccess) {
+		fprintf(stderr, "cudaMalloc failed!");
+		goto Error;
+	}
+
+	// in
+	cudaStatus = cudaMalloc((void**)&dev_a, size * sizeof(double));
+	if (cudaStatus != cudaSuccess) {
+		fprintf(stderr, "cudaMalloc failed!");
+		goto Error;
+	}
+
+	// Copy inputs from Host -> GPU Buffer
+	cudaStatus = cudaMemcpy(dev_a, a, size * sizeof(double), cudaMemcpyHostToDevice);
+	if (cudaStatus != cudaSuccess) {
+		fprintf(stderr, "cudaMemcpy failed!");
+		goto Error;
+	}
+
+
+	// Launch kernel w/ one thread per element.
+	func << <1, size >> > (dev_b, dev_a);
+
+	// Get kernel launch errors
+	cudaStatus = cudaGetLastError();
+	if (cudaStatus != cudaSuccess) {
+		fprintf(stderr, "addKernel launch failed: %s\n", cudaGetErrorString(cudaStatus));
+		goto Error;
+	}
+
+	// Wait for kernel finish
+	cudaStatus = cudaDeviceSynchronize();
+	if (cudaStatus != cudaSuccess) {
+		fprintf(stderr, "cudaDeviceSynchronize returned error code %d after launching addKernel!\n", cudaStatus);
+		goto Error;
+	}
+
+	// Copy outputs from GPU Buffer -> Host
+	cudaStatus = cudaMemcpy(b, dev_b, size * sizeof(double), cudaMemcpyDeviceToHost);
+	if (cudaStatus != cudaSuccess) {
+		fprintf(stderr, "cudaMemcpy failed!");
+		goto Error;
+	}
+
+Error:
+	cudaFree(dev_b);
+	cudaFree(dev_a);
+
+	return cudaStatus;
+}
